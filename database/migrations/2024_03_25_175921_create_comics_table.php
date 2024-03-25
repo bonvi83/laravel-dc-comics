a8532ef041dd84aa1a1dc0a2bbfe151d23d29fdd
bonvi83 <bonvi83@gmail.com>
2024-03-25 19:02:44 +0100

    add controller and seeder

---
+++ database/migrations/2024_03_25_175921_create_comics_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+            $table->string("title", 50);
+            $table->text("description");
+            $table->string("thumb", 100);
+            $table->tinyInteger("price");
+            $table->string("series", 50);
+            $table->dateTime("sale_date");
+            $table->string("type", 30);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comics');
+    }
+};
